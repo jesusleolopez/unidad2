acd844f5f83ab3b615ce8643b3eee6e84ed094a7
jesusleolopez <143725113+jesusleolopez@users.noreply.github.com>
2023-09-07 08:50:04 -0600

    Add files via upload

---
+++ Practica3.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica2;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+
+   
+    public static void main(String[] args) {
+        
+        String nombre = "Jesus David Leocadio Lopez";
+        
+        int base = 3;
+        int altura = 5;
+         
+        int areaRectangulo = base * altura;
+        
+        System.out.println("mi nombre es : " + nombre);
+                System.out.println(areaRectangulo);
+
+                
+    }
+    
+}
